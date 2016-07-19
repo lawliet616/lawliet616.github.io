7614ce67e50c79d819f07c64e68ae2f58d9ebf56
Valentin Krasznai <krasznai.valentin@openminds.hu>
2016-07-19 12:22:27 +0200

    asd
    asd

--- networkchange/js/main.js
+++ networkchange/js/main.js
@@ -3,32 +3,17 @@ $(document).ready(function($) {
     console.log(navigator);
 
     var supported = $("#supported");
-    var oldtype = $("#old-type");
-    var bandwith = $("#old-bandwith");
     var type = $("#new-type");
 
     var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection || null;
 
     if(connection == null) {
-        supported.text("API not supported");
-    } else if ('metered' in connection) {
-        supported.text("Old API supported");
-        
-        function changeHandler(){
-            bandwith.text(connection.bandwidth);
-            oldtype.text((connection.metered ? '' : 'not ') + 'metered');
-            console.log(connection.type);
-        }
-
-        changeHandler();
-        connection.addEventListener('typechange', changeHandler);
-        connection.addEventListener('change', changeHandler);
-
+        supported.text("NetworkInformation API not supported");
     } else {
-        supported.text("API supported");
+        supported.text("NetworkInformation API supported");
         
         function changeHandler(){
-            type.text(connection.type);
+            type.text('Connection type: ' + connection.type);
             console.log(connection.type);
         }
 
